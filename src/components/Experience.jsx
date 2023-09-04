9aff70dd684b3fae165ebacdd178478f3bb2f6fd
Shuvro Pal <shuvropal@gmail.com>
2023-09-05 00:57:58 +0600

    Experience component height issue fixed.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,8 +4,8 @@ import ExperienceItem from './ExperienceItem';
 
 const Experience = () => {
   return (
-    <div name='experience' className='w-full md:h-screen bg-[#0F1729] text-[#f9fafc]'>
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+    <div name='experience' className='w-full bg-[#0F1729] text-[#f9fafc]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-[#f9fafc] text-[#f9fafc]'>Working Experience</p>
         </div>
